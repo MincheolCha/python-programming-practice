f3400bf0a6a663099c430107a4f23f2c030ea88e
minchul47 <mccha47@hanmail.net>
2016-08-24 11:22:53 +0900

    Chapter 4 finish

--- Basic Data 02.py
+++ Basic Data 02.py
@@ -4,3 +4,21 @@ b = '''hello
 world'''
 print(a)
 print(b)
+
+#Slicing, Access, in연산자
+print(a[1])
+print(a[:5])
+print('Hello' in  a)
+print('hello' in  a)
+print('length = ' + str(len(a)))
+
+#Format
+c = 'My name is {0}. I\'m {1} years old.'
+print(c.format('Mincheol',27))
+d = "My name is {name}. I'm {age} years old."
+print(d.format(name='Mincheol',age=27))
+print(name)
+print(age)
+
+#int(), float(), complex(), str()
+#논리 연산자, <<. >>, &, |, ^, ~
